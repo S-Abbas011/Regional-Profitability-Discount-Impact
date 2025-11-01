463b045898f393607ada4e635c7d3cfc21697ea0
Syed Tasauwar Abbas <syeddabbas011@gmail.com>
2025-11-01 06:41:54 +0530

    Update and rename Solution.txt to Solution.txt.sql

--- Solution.txt.sql
+++ Solution.txt.sql
@@ -1,10 +1,10 @@
 Business Question:
 How do discounts affect profitability across regions and product categories?
 SQL Task:
-•   Join Orders and Regions.
-•   Compute correlation between Discount and Profit.
-•   Group by Region and Category.
-•   Flag regions where higher discounts reduce overall profit.'
+â€¢ Â  Join Orders and Regions.
+â€¢ Â  Compute correlation between Discount and Profit.
+â€¢ Â  Group by Region and Category.
+â€¢ Â  Flag regions where higher discounts reduce overall profit.'
 
 WITH region_profit AS (
 select c.state AS region,p.category,avg(s.discount) AS avg_discount,
@@ -21,10 +21,10 @@ group by c.state, p.category
 flagged_regions AS (
 select region,category,avg_discount,avg_profit,discount_profit_corr,
 CASE 
-    WHEN discount_profit_corr < 0 THEN '?? Discount reduces profit'
-    WHEN discount_profit_corr > 0 THEN '? Discount increases profit'
-    ELSE 'No clear relationship'
-    END as discount_impact_flag
+Â  Â  WHEN discount_profit_corr < 0 THEN '?? Discount reduces profit'
+Â  Â  WHEN discount_profit_corr > 0 THEN '? Discount increases profit'
+Â  Â  ELSE 'No clear relationship'
+Â  Â  END as discount_impact_flag
 from region_profit
 )
 SELECT *
